d794d1d4afa77ce2471ff5cbe7969c69af8c74de
Dawid Dziurdzia <nazgu1@users.noreply.github.com>
2024-01-18 19:48:46 +0100

    Initial Commit

---
+++ Orchestra/OrchestraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchestraApp.swift
+//  Orchestra
+//
+//  Created by Dawid Dziurdzia on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct OrchestraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
